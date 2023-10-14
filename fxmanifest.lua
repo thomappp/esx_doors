4b9017ac4d9f41f21c519df4a7f244d263c309c2
thomappp <143356129+thomappp@users.noreply.github.com>
2023-10-14 10:51:52 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'Thomapp'
 description 'Script for locking and unlocking doors.'
 
 shared_scripts { '@es_extended/imports.lua', 'config.lua' }
-client_script 'cl_door.lua'
-server_script 'sv_door.lua'
+client_script 'client.lua'
+server_script 'server.lua'
 
 dependency 'es_extended'
